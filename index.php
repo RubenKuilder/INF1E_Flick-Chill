01e58f38056e57b776e1e7e09901822c7e7a1341
Jurrian Tanke <jurriantanke@hotmail.com>
2018-12-03 14:27:28 +0100

    login scherm

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
-<?php
-$configs = include('system/config.php');
-?>
-
 <!DOCTYPE html>
+<?php 
+//$configs = include('system/config.php');
+?>
 <html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-		<h1>Hello world!</h1>
-	</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/login.css">
+    </head>
+    <body>
+        <div id="loginWrapper">
+            <div id="loginLogo">
+                <img src="assets/images/logo.png" alt="logo">
+            </div>
+            <div id="loginForm">
+                <p>
+                    Log hier in met je schoolgegevens:
+                </p>
+                <form action="system/login.php" method="post">
+                    <input type="email" name="email" placeholder="Email..."><br/>
+                    <input type="password" name="password" placeholder="Password"><br/>
+                    <input type="submit" name="submit" value="LOG IN">
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
