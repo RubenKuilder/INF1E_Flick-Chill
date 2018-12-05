9eb1d9b5010cf515913b03658fb574e96b510ec1
Jurrian Tanke <jurriantanke@hotmail.com>
2018-12-05 12:48:07 +0100

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<?php
+//require('system/confiq.php');
+//require('system/header.php');
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FlickAndChill</title>
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/dashboard.css">
+    </head>
+    <body>
+        <div class="flex-container">
+            <div class="titleID">
+                <h2>Popular</h2>
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+            <div class="iframeID">
+            </div>
+        </div>
+    </body>
+</html>
