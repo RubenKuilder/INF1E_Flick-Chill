a8f9e4cb8cbfc363f37969b13dc8912ffe77318b
Ruben Kuilder <rubenkuilder@gmail.com>
2018-12-05 09:54:13 +0100

    Changed gitignore config file

--- system/config.php
+++ system/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = 'localhost';
 $username = "root";
-$password = "";
+$password = "root";
 $databasename = 'Flick-Chill' ;
 $conn = new mysqli($servername, $username, $password, $databasename);
 
