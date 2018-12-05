add518918ee03c7f13dc9c44a6b1dd34da6cc3b7
Jordy Neef <43172462+JordyNeef@users.noreply.github.com>
2018-12-05 20:56:20 +0100

    header v0,8

--- header.php
+++ header.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link rel="stylesheet" type="text/css" href="assets/stylesheets/head.css">
-    <head>
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/head.css">
+        <script>
+            function myFunction(x) {
+                x.classList.toggle("change");
+            }
+        </script>
+    </head>
     <body>
     <div class="head">
         <ul>
             <li>
                 <div class="menu" onclick="myFunction(this)">
-                    <script>
-                        function myFunction(x) {
-                            x.classList.toggle("change");
-                        }
-                    </script>
                     <div class="bar1"></div>
                     <div class="bar2"></div>
                     <div class="bar3"></div>
                 </div>
             </li>
             <li>
-                <img class="logo" src="assets/images/logo.png" alt='Logo'>
+                <div class="logo">
+                    <img class="size" src="assets/images/logo.png" alt='Logo'>
+                </div>
             </li>
             <li>
-                <form  value="search" action="post">
-                    <input class="searchbar" type="text" name="searchbar" placeholder="Search">
-                </form>
+                    <input class="searchbar" type="text" name="searchbar" placeholder="&#9773; Search...">
             </li>
             <li>
                 <div class="border">
