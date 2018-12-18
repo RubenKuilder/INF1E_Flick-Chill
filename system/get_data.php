1a57c51476f1d43e827e56d20222e04d10a52808
Ruben Kuilder <rubenkuilder@gmail.com>
2018-12-18 14:05:13 +0100

    Added card divs creation to get_data.php

--- system/get_data.php
+++ system/get_data.php
@@ -17,14 +17,27 @@
         }
     }
     
-    mysqli_stmt_num_rows($stmt, $VideoID, $UserID, $isApp, $isLive, $Description, $URL)
+    mysqli_stmt_num_rows($stmt, $VideoID, $UserID, $isApp, $isLive, $Description, $URL, $Thumbnail $Title)
     mysqli_stmt_store_result($stmt);
     
     if(mysqli_stmt_num_rows($stmt) > 0)
     {
         while(mysqli_stmt_fetch($stmt))
         {
-            echo ""
+            echo "
+            <div class='card' style='background-image: url(../assets/images/uploads/". $Thumbnail .");'>
+                <div class='overlay'>
+                    <div class='overlayTextContainer'>
+                        <h2>". $Title ."</h2>
+                        <p>". $Description ."</p>
+                    </div>
+                    <div class='ratingContainer'>
+                        <span class='rateLike'></span>
+                        <span class='rateNeutral'></span>
+                        <span class='rateDislike'></span>
+                    </div>
+                </div>
+            </div>";
         }
     }
 ?>
