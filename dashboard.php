5f6a021d68da122a33275ae20d337b94e3058c1b
Ruben Kuilder <rubenkuilder@gmail.com>
2018-12-18 13:58:45 +0100

    New dashboard and fixed some login and register issues.

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 //require('system/confiq.php');
-require('system/header.php');
+//require('system/header.php');
 
 ?>
 <html>
@@ -27,7 +27,26 @@ require('system/header.php');
         </script>
     </head>
     <body>
-        <div class="flex-container" data-infinite-scroll='{ "path": ".pagination__next", "append": ".post", "history": false }'>
+    <h1 id="dashboardTitle">Dashboard</h1>
+    <section id="dashboardContainer">
+        <div class="card" style="background-image: url(https://media.allure.com/photos/59dd28498ba03d6a578a1cd0/16:9/w_1280/emma%2520watson.jpg);">
+            <div class="overlay">
+                <div class="overlayTextContainer">
+                    <h2>Overlay title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="ratingContainer">
+                    <span class="rateLike"></span>
+                    <span class="rateNeutral"></span>
+                    <span class="rateDislike"></span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<!--         <div class="flex-container" data-infinite-scroll='{ "path": ".pagination__next", "append": ".post", "history": false }'>
             <div class="iframeID">
                 <img src="assets/images/emma-watson.jpg">
                 <div class="overlay">
@@ -46,6 +65,6 @@ require('system/header.php');
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </body>
 </html>
--- system/register.php
+++ system/register.php
@@ -13,20 +13,7 @@ $postLname = $_POST['lname'];
 $postEmail = $_POST['email'];
 $postPass = $_POST['pass'];
 
-if (CRYPT_BLOWFISH == 1){
-    $items = array('A','b','1','24','3','Y','j');
-    $salt = '';
-    for($i = 0; $i < 30; $i++){
-        $random = rand(0, sizeof($items));
-        $salt = $salt.$items[$random];
-    }
-    //                echo "salt:".$salt;
-    $hash = crypt($_POST['pass'],'$2a$09$'.$salt.'$');
-    //                echo "Blowfish: ".$hash."\n<br>";
-} else{
-    //echo "Blowfish DES not supported.\n<br>";
-    echo 'blowfishError';
-}
+$hashed = password_hash($postPass, PASSWORD_DEFAULT);
 
 //-----------------------------
 // Step #3: Create the query
@@ -52,6 +39,8 @@ else{
     die(mysqli_error($conn));
 }
 
+echo $hashed;
+
 // Step #6.1: Bind result to variables when fetching...
 mysqli_stmt_bind_result($statement, $postEmail);
 // Step #6.2: And buffer the result if you want to display the data
@@ -63,10 +52,10 @@ if(mysqli_stmt_num_rows($statement) > 0)
     echo 'A user with the same email already exists.';
 }
 else {
-    $sql = "INSERT INTO account (firstname, lastname, email, password, pass_crypt) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO account (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
 
 	if ($stmt = mysqli_prepare($conn, $sql)) {
-		mysqli_stmt_bind_param($stmt, 'sssss', $postFname, $postLname, $postEmail, $hash, $salt);
+		mysqli_stmt_bind_param($stmt, 'ssss', $postFname, $postLname, $postEmail, $hashed);
 
 		if (mysqli_stmt_execute($stmt)) {
 			echo "Query executed";
