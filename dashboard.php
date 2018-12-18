537dc94fd8a3760d760003df33e4d5c27f1e53b9
Jurrian Tanke <jurriantanke@hotmail.com>
2018-12-18 13:50:11 +0100

    fix

--- dashboard.php
+++ dashboard.php
@@ -10,69 +10,20 @@ require('system/header.php');
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>FlickAndChill</title>
         <link rel="stylesheet" type="text/css" href="assets/stylesheets/dashboard.css">
-        <script>
-        function loadResults() {
-            var result = "";
-            for (var i = 0; i < 10; i++) {
-                result += "<div class="flex-container" data-infinite-scroll='{ "path": ".pagination__next", "append": ".post", "history": false }'>
-            <div class="iframeID">
-                <img src="assets/images/emma-watson.jpg">
-                <div class="overlay">
-                    <div class="text-content">
-                        <h2>Titel</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut...</p>
-                    </div>
-                    <div class="rating">
-                        <div class="green">
-                        </div>
-                        <div class="white">
-                        </div>
-                        <div class="red">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>";
-            }
-            $.ajax({
-                url: "/echo/html/",
-                type: "post",
-                data: {
-                    html: result,
-                    delay: 3
-                },
-                beforeSend: function(xhr) {
-                    $("#results").after($("<li class='loading'>Loading...</li>").fadeIn('slow')).data("loading", true);
-                },
-                success: function(data) {
-                    var $results = $("#results");
-                    $(".loading").fadeOut('fast', function() {
-                        $(this).remove();
-                    });
-                    var $data = $(data);
-                    $data.hide();
-                    $results.append($data);
-                    $data.fadeIn();
-                    $results.removeData("loading");
-                }
-            });
-        };
-        $(function() {
-            loadResults();
-
-            $(".scrollpane").scroll(function() {
-                var $this = $(this);
-                var $results = $("#results");
-
-                if (!$results.data("loading")) {
-
-                    if ($this.scrollTop() + $this.height() == $results.height()) {
-                        loadResults();
-                    }
-                }
-            });
-        });
+        <script type='text/javascript' src='system/get_data.php'></script>
+        <script type='text/javascript'>
+            $(document).ready(function()
+            {
+                $.ajax(
+                {
+                    url:'get_data.php',
+                    data:
+                    {
+                        'offset': 0,
+                        'limit': 10
+                    },
+                });              
+            }):
         </script>
     </head>
     <body>
--- system/get_data.php
+++ system/get_data.php
@@ -1,8 +1,30 @@
 <?php
+    $limit = $_GET['limit'];
+    $offset = $_GET['offset'];
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    $tableName = 'video';
+    $selectQuery = "SELECT * FROM " . $tableName;
+    if($stmt = mysqli_prepare($conn, $selectQuery))
+    {
+        if(!mysqli_execute($stmt))
+        {
+            echo "Error executing query";
+            die(mysqli_error($conn));
+        }
+        else
+        {
+            die(mysqli_error($conn));
+        }
+    }
+    
+    mysqli_stmt_num_rows($stmt, $VideoID, $UserID, $isApp, $isLive, $Description, $URL)
+    mysqli_stmt_store_result($stmt);
+    
+    if(mysqli_stmt_num_rows($stmt) > 0)
+    {
+        while(mysqli_stmt_fetch($stmt))
+        {
+            echo ""
+        }
+    }
+?>
