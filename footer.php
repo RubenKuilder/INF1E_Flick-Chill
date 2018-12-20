5222f1aee739c97ccc5ffce9fabf2b1ebdb7b695
JordyNeef <43172462+JordyNeef@users.noreply.github.com>
2018-12-20 17:11:19 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+        <div class="footer">
+            <div class="left">
+                <p><i>&#169; NHLStenden, 2018-2019.</i></p>
+                <p class="made"><i>&nbsp;&nbsp; Made by, Undercover Apple and the Rap Goblins</i></p>
+            </div>
+            <div class="right">
+                <p>
+                    <a href="https://www.facebook.com"><img class="social" src="assets/images/facebook.png"></a>
+                    <a href="https://twitter.com"><img class="social" src="assets/images/twitter.png"></a></p>
+                    <a href="https://instagram.com"><img class="social" src="assets/images/instagram.png"></a>
+                    <a href="https://youtube.com"><img class="social" src="assets/images/youtube.png"></a>
+                    <a href="https://linkedin.com"><img class="social" src="assets/images/linkedin.png"></a>
+                </p>
+            </div> 
+        </div>
+    </body>
+</html>
--- header.php
+++ header.php
@@ -2,8 +2,9 @@
 <html lang="en">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <meta name="viewport" content="height=device-height, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="assets/stylesheets/header.css">
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/footer.css">
         <script> //hamburger menu script
             function ourFunction(x) {
                 x.classList.toggle("change");
@@ -13,35 +14,35 @@
     </head>
     <body>
     <!--begin body-->
-    <div class="header">
-        <!--begin header-->
-        <ul>
-            <li>
-                <div class="menu" onclick="ourFunction(this)">
-                    <div class="bar1"></div>
-                    <div class="bar2"></div>
-                    <div class="bar3"></div>
-                </div>
-            </li>
-            <li>
-                <div class="logo">
-                    <img class="size" src="assets/images/logo_header.png" alt='Logo'>
-                </div>
-            </li>
-            <li>
-                <div class="user">
-                    <p>Naam gebruiker<p>
-                </div>
-            </li>
-            <li>
-                <div class="border">
-                </div>
-            </li>
-            <li>
-                <div class="bar">
-                    <input class="searchbar" type="text" name="searchbar" placeholder="&#x1F50E; &nbsp;Zoeken...">
-                </div>
-            </li>
-        </ul>
-    </div>
-    <!--eind header-->
+        <div class="header">
+            <!--begin header-->
+            <ul>
+                <li>
+                    <div class="menu" onclick="ourFunction(this)">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
+                </li>
+                <li>
+                    <div class="logo">
+                        <img class="size" src="assets/images/logo_header.png" alt='Logo'>
+                    </div>
+                </li>
+                <li>
+                    <div class="user">
+                        <p>Naam gebruiker<p>
+                    </div>
+                </li>
+                <li>
+                    <div class="border">
+                    </div>
+                </li>
+                <li>
+                    <div class="bar">
+                        <input class="searchbar" type="text" name="searchbar" placeholder="&#x1F50E; &nbsp;Zoeken...">
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!--eind header-->
