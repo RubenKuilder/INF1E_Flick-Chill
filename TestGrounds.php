3b87a243ee1a6455c197758806d5714e7a1f2688
Jurrian Tanke <jurriantanke@hotmail.com>
2019-01-09 10:17:56 +0100

    popuptest

---
+++ TestGrounds.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/stylesheets/TestGrounds.css">
+    </head>
+    <body>
+        <div class='popUpWrapper'>
+            <div class='popUp'>
+                <div class='iFrame'>
+                    <iframe scr="https://youtu.be/737dCFBqO4I"></iframe>
+                </div>
+                <div class='description'>
+                    <h2>Apple Killed the Mac Mini</h2>
+                    <p>
+                        It’s been YEARS since Apple has released an update to the Mac Mini, the most AFFORDABLE Mac available… But it’s not so affordable anymore. So who’s it for now?
+                    </p>
+                </div>
+                <div class='url'>
+                    <a href='https://www.youtube.com/watch?v=737dCFBqO4I'>https://www.youtube.com/watch?v=737dCFBqO4I</a>
+                </div>               
+            </div>           
+        </div>
+    </body>
+</html>
